90278509e500b7edd19731cb3bafbbd777784fb4
hbina <hanif.ariffin.4326@gmail.com>
2018-08-23 19:01:08 -0400

    trying to work out db stuff

--- src/main/java/com/akarin/webapp/managers/DatabaseManager.java
+++ src/main/java/com/akarin/webapp/managers/DatabaseManager.java
@@ -1,8 +1,14 @@
 package com.akarin.webapp.managers;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -20,6 +26,10 @@ public class DatabaseManager {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseManager.class);
     private static String DATABASE_USERNAME = "postgres";
     private static String DATABASE_PASSWORD = "1234";
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Autowired
+    private DataSource dataSource;
 
     public static void setDbLogin() {
         try {
@@ -44,15 +54,29 @@ public class DatabaseManager {
         String dbUrl;
 
         if (System.getenv("DATABASE_URL") != null) {
+            logger.debug("DATABASE_URL FOUND");
             final URI dbUri = new URI(System.getenv("DATABASE_URL"));
             username = dbUri.getUserInfo().split(":")[0];
             password = dbUri.getUserInfo().split(":")[1];
             dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
         } else {
-            // If an .env file is not provided, try to use a local psql database instead
+            logger.debug("DATABASE_URL NOT FOUND: .env file is not provided, try to use a local psql database instead");
             dbUrl = "jdbc:postgresql://" + "localhost" + ":" + "5432" + "/" + username;
         }
 
         return DriverManager.getConnection(dbUrl, username, password);
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
     }
 }
